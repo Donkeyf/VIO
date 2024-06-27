641f6032719f816ecf972a4f9107c50614f4df08
DilonH <dilonhewamanna@outlook.com>
2024-06-27 13:13:49 +1000

    cpp

---
+++ CPP Pose Estimation/PnP.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+class PnP
+{
+public:
+    explicit PnP(c);
+    virtual ~PnP();
+
+
+}
---
+++ GateDetection.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/core/utils/filesystem.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/video/tracking.hpp>
+
+using namespace cv;
+using namespace std;
